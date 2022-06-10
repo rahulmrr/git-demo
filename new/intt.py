33e5bc588fb7c3157f400c48e5198226f5effede
Rahul M R <rahuljagur78@gmail.com>
2022-06-10 23:43:10 +0530

    vs code commit

---
+++ new/intt.py
@@ -0,0 +1,20 @@
+import re
+print(re.findall("\d","abc123"))
+print(re.findall("\w","abc123"))
+print(re.findall("\W","@#$%asd987"))
+print(re.findall("\d","@#$%asd987"))
+print(re.findall("\d","@#$%asd987"))
+final_word_found = ["help","abc123","PromoRevoooooooooooo","123",".jp7g","abs","321",".247","@#$%asd987","$fdf","oju*"]
+a = [word for word in final_word_found if len(re.findall("\d",word))==0 if len(re.findall("\W",word))==0]
+print(a)
+#print(re.findall("{\}","@#$%asd987"))
+#final_word_found = ["help","abc123","123",".jpg","abs","321",".247","@#$%asd987"]
+#[word for word in b if re.findall()]
+##if len(re.findall("\W",word)
+
+
+c=8
+b=5
+print(c+b)
+
+
--- new/newcommands.md
+++ new/newcommands.md
@@ -1,3 +1,3 @@
 uhweidfugh2fh
 wefkjgqwifuchbqpfv
-vq3lmgr wrgvgmwgv wmvw v
+vq3lmgr wrgvgmwgv wmvw vC:\Users\Rahul M R\New folder\git-demo\new\newcommands.md
